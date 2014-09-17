f36f2ae9a4611574bfe3328e8c73df3f35f25f9c
David Sam <chsam27@aol.com>
2014-09-17 12:16:30 -0400

    Added JSON

--- GradleSlideshow/src/main/webapp/js/message.js
+++ GradleSlideshow/src/main/webapp/js/message.js
@@ -5,7 +5,7 @@
 //you can write a dependancy for jquery here if you want like.... define(['jquery'])
 define(['jquery'], function($){
 	var message = "";
-	
+	// just a loop
 	for(var i = 0; i < 10; i++){
 		message += "<p> SOME STUFF GOES HERE </p>"
 	}
